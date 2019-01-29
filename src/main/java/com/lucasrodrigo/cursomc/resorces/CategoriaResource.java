7b0772cdc125907a9cca99ea20f099630e920b04
Lucas Rodrigo Frederico <lukinhasdayz@hotmail.com>
2019-01-29 16:55:59 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/lucasrodrigo/cursomc/resorces/CategoriaResource.java
+++ src/main/java/com/lucasrodrigo/cursomc/resorces/CategoriaResource.java
@@ -9,10 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
@@ -23,7 +19,6 @@ public class CategoriaResource {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id) {
         Categoria obj = service.buscar(id);
-
         return ResponseEntity.ok().body(obj);
     }
 
--- src/main/java/com/lucasrodrigo/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasrodrigo/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.lucasrodrigo.cursomc.services;
 
 import com.lucasrodrigo.cursomc.domain.Categoria;
 import com.lucasrodrigo.cursomc.repositories.CategoriaRepository;
+import com.lucasrodrigo.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
