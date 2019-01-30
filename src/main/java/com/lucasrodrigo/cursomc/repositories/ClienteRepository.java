2cd02d0f1b77b47b284860760b77f1f74194953c
Lucas Rodrigo Frederico <lukinhasdayz@hotmail.com>
2019-01-30 10:31:48 -0200

    Cliente, TipoCliente, telefones e endereços

---
+++ src/main/java/com/lucasrodrigo/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.lucasrodrigo.cursomc.repositories;
+
+import com.lucasrodrigo.cursomc.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+
+}
---
+++ src/main/java/com/lucasrodrigo/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.lucasrodrigo.cursomc.repositories;
+
+import com.lucasrodrigo.cursomc.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+
+}
