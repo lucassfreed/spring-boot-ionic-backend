c4af5e6c64cc601bc7b76ef9f8469a91e3369283
Lucas Rodrigo Frederico <lukinhasdayz@hotmail.com>
2019-01-30 11:33:06 -0200

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/lucasrodrigo/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.lucasrodrigo.cursomc.repositories;
+
+import com.lucasrodrigo.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+
+}
