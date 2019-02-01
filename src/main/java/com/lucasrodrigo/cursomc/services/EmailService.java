244d53997e6ea179f079fa1f1d1e04c1868fd15e
Lucas Rodrigo Frederico <lukinhasdayz@hotmail.com>
2019-02-01 15:40:10 -0200

    Email HTML

--- src/main/java/com/lucasrodrigo/cursomc/services/EmailService.java
+++ src/main/java/com/lucasrodrigo/cursomc/services/EmailService.java
@@ -3,9 +3,15 @@ package com.lucasrodrigo.cursomc.services;
 import com.lucasrodrigo.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
