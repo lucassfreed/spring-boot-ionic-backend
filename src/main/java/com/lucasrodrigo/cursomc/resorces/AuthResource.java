af4db74832e56e4d36a4b8cd087e6a268a58e4f0
Lucas Rodrigo Frederico <lukinhasdayz@hotmail.com>
2019-02-04 14:00:12 -0200

    Esqueci a senha

--- src/main/java/com/lucasrodrigo/cursomc/resorces/AuthResource.java
+++ src/main/java/com/lucasrodrigo/cursomc/resorces/AuthResource.java
@@ -1,15 +1,19 @@
 package com.lucasrodrigo.cursomc.resorces;
 
+import com.lucasrodrigo.cursomc.dto.EmailDTO;
 import com.lucasrodrigo.cursomc.security.JWTUtil;
 import com.lucasrodrigo.cursomc.security.UserSS;
+import com.lucasrodrigo.cursomc.services.AuthService;
 import com.lucasrodrigo.cursomc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -18,6 +22,9 @@ public class AuthResource {
     @Autowired
     private JWTUtil jwtUtil;
 
+    @Autowired
+    private AuthService service;
+
     @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
     public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
         UserSS user = UserService.authenticated();
@@ -25,5 +32,11 @@ public class AuthResource {
         response.addHeader("Authorization", "Bearer " + token);
         return ResponseEntity.noContent().build();
     }
-    
+
+    @RequestMapping(value = "/forgot", method = RequestMethod.POST)
+    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+        service.sendNewPassword(objDto.getEmail());
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/lucasrodrigo/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/lucasrodrigo/cursomc/services/AbstractEmailService.java
@@ -1,5 +1,6 @@
 package com.lucasrodrigo.cursomc.services;
 
+import com.lucasrodrigo.cursomc.domain.Cliente;
 import com.lucasrodrigo.cursomc.domain.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,6 +40,23 @@ public abstract class AbstractEmailService implements EmailService {
         sm.setText(obj.toString());
         return sm;
     }
+
+    @Override
+    public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+        SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+        sendEmail(sm);
+    }
+
+    protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+        SimpleMailMessage sm = new SimpleMailMessage();
+        sm.setTo(cliente.getEmail());
+        sm.setFrom(sender);
+        sm.setSubject("Solicitação de nova senha");
+        sm.setSentDate(new Date(System.currentTimeMillis()));
+        sm.setText("Nova senha: " + newPass);
+        return sm;
+    }
+
 
     protected String htmlFromTemplatePedido(Pedido obj) {
         Context context = new Context();
