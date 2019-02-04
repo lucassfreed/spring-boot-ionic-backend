6ffcacfbcf61fdee483390d3200d4494bad0ae52
Lucas Rodrigo Frederico <lukinhasdayz@hotmail.com>
2019-02-04 10:59:40 -0200

    Autorizando endpoints para perfis específicos

--- src/main/java/com/lucasrodrigo/cursomc/resorces/ClienteResource.java
+++ src/main/java/com/lucasrodrigo/cursomc/resorces/ClienteResource.java
@@ -7,6 +7,7 @@ import com.lucasrodrigo.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -45,12 +46,14 @@ public class ClienteResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<ClienteDTO>> findAll() {
         List<Cliente> list = service.findAll();
